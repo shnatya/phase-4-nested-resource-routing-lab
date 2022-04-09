cdd7369b63415f0fab0ab7a7149df965a4801a6e
Anastasia Shatokhina <shnatya@gmail.com>
2022-04-09 10:46:10 -0700

    commit tree 5b98dfd2c3a0bc1a72dd8aadb3da200845370b84

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,37 @@
 class ItemsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  #GET /items or /users/:user_id/items 
   def index
-    items = Item.all
+    if params[:user_id] 
+      user = User.find(params[:user_id])
+      items = user.items
+    else 
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  #GET /users/:user_id/items/:id
+  def show
+    user = User.find(params[:user_id])
+    item = user.items.find(params[:id])
+    render json: item
+  end
+
+  #POST /users/:user_id/items
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response
+    render json: {error: "User not found"}, status: :not_found
+  end
 end
